ac5316e9d8f118f5a4b970344e2ec0435f3139b8
kapitantilapia <johnmatsola@gmail.com>
2023-05-14 02:42:19 +0800

    Removed drag functionality - fixed animation

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   const spirographContainer = document.getElementById("spirograph-container");
   const planets = [
-    { name: "Mercury", color: "gray", radius: 5, orbitRadius: 50, speed: 0.005 },
-    { name: "Venus", color: "orange", radius: 10, orbitRadius: 80, speed: 0.003 },
-    { name: "Earth", color: "blue", radius: 12, orbitRadius: 120, speed: 0.002 },
-    { name: "Mars", color: "red", radius: 8, orbitRadius: 160, speed: 0.0015 },
-    { name: "Jupiter", color: "brown", radius: 25, orbitRadius: 220, speed: 0.001 },
-    { name: "Saturn", color: "tan", radius: 22, orbitRadius: 280, speed: 0.0008 },
-    { name: "Uranus", color: "lightblue", radius: 18, orbitRadius: 340, speed: 0.0006 },
-    { name: "Neptune", color: "darkblue", radius: 16, orbitRadius: 400, speed: 0.0005 },
+    { name: "Sun", color: "yellow", radius: 15, orbitRadius: 0, speed: 0 },
+    { name: "Mercury", color: "gray", radius: 5, orbitRadius: 50, speed: 5 },
+    { name: "Venus", color: "orange", radius: 10, orbitRadius: 80, speed: 3 },
+    { name: "Earth", color: "blue", radius: 12, orbitRadius: 120, speed: 2 },
+    { name: "Mars", color: "red", radius: 8, orbitRadius: 160, speed: 1.5 },
+    { name: "Jupiter", color: "brown", radius: 25, orbitRadius: 220, speed: 1 },
+    { name: "Saturn", color: "tan", radius: 22, orbitRadius: 280, speed: 0.8 },
+    { name: "Uranus", color: "lightblue", radius: 18, orbitRadius: 340, speed: 0.6 },
+    { name: "Neptune", color: "darkblue", radius: 16, orbitRadius: 400, speed: 0.5 },
   ];
 
   let angle = 0;
   let zoomLevel = 1;
+
+  // Zoom in button
+  const zoomInButton = document.getElementById("zoom-in-button");
+  zoomInButton.addEventListener("click", function () {
+    zoomLevel += 0.1; // Increase zoom level
+  });
+
+  // Zoom out button
+  const zoomOutButton = document.getElementById("zoom-out-button");
+  zoomOutButton.addEventListener("click", function () {
+    zoomLevel -= 0.1; // Decrease zoom level
+    if (zoomLevel < 0.1) {
+      zoomLevel = 0.1; // Set minimum zoom level
+    }
+  });
 
   function animate() {
     // Clear the container
@@ -22,25 +38,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const containerWidth = spirographContainer.offsetWidth;
     const containerHeight = spirographContainer.offsetHeight;
 
-  // Draw circle outlines for planet orbits
-planets.forEach(function (planet) {
+    // Add the sun
+    const sunElement = document.createElement("div");
+    sunElement.className = "sun";
+    spirographContainer.appendChild(sunElement);
+
+    // Draw planets
+planets.forEach(function (planet, index) {
   const orbitRadius = planet.orbitRadius * zoomLevel;
   const planetRadius = planet.radius * zoomLevel;
 
-  const orbitX = containerWidth / 2 - planetRadius; // Updated calculation
-  const orbitY = containerHeight / 2 - planetRadius; // Updated calculation
-
-  const orbitElement = document.createElement("div");
-  orbitElement.className = "orbit";
-  orbitElement.style.width = orbitRadius * 2 + "px";
-  orbitElement.style.height = orbitRadius * 2 + "px";
-  orbitElement.style.top = orbitY + "px";
-  orbitElement.style.left = orbitX + "px";
-
-  spirographContainer.appendChild(orbitElement);
-
-  const planetX = orbitX + orbitRadius + Math.cos(angle) * orbitRadius - planetRadius;
-  const planetY = orbitY + orbitRadius + Math.sin(angle) * orbitRadius - planetRadius;
+  const planetX =
+    containerWidth / 2 +
+    Math.cos(angle * planet.speed) * (orbitRadius + planet.orbitRadius * zoomLevel) -
+    planetRadius;
+  const planetY =
+    containerHeight / 2 +
+    Math.sin(angle * planet.speed) * (orbitRadius + planet.orbitRadius * zoomLevel) -
+    planetRadius;
 
   const planetElement = document.createElement("div");
   planetElement.className = "planet";
@@ -53,72 +68,17 @@ planets.forEach(function (planet) {
   const planetName = document.createElement("div");
   planetName.className = "planet-name";
   planetName.textContent = planet.name;
+  planetName.style.fontSize = planetRadius / 4 + "px"; // Adjust the font size relative to the planet's size
+  planetName.style.top = planetRadius * 2 + "px"; // Position the label below the planet
 
   planetElement.appendChild(planetName);
   spirographContainer.appendChild(planetElement);
-
-  angle += planet.speed;
 });
-// Add the Sun at the center
-const sunElement = document.createElement("div");
-sunElement.className = "sun";
-spirographContainer.appendChild(sunElement);
+
+    angle += 0.01; // Increase the angle for smoother animation
+
     requestAnimationFrame(animate);
   }
-
-  // Zoom in button
-  const zoomInButton = document.getElementById("zoom-in-button");
-  zoomInButton.addEventListener("click", function () {
-    zoomLevel += 0.1; // Increase zoom level
-  });
-
-    // Zoom out button
-    const zoomOutButton = document.getElementById("zoom-out-button");
-    zoomOutButton.addEventListener("click", function () {
-      zoomLevel -= 0.1; // Decrease zoom level
-      if (zoomLevel < 0.1) {
-        zoomLevel = 0.1; // Set minimum zoom level
-      }
-    });
-    
- // Drag functionality
-let isDragging = false;
-let startPosition = { x: 0, y: 0 };
-let currentTranslate = { x: 0, y: 0 };
-
-spirographContainer.addEventListener("mousedown", function (event) {
-  isDragging = true;
-  startPosition.x = event.clientX - currentTranslate.x;
-  startPosition.y = event.clientY - currentTranslate.y;
-});
-
-spirographContainer.addEventListener("mousemove", function (event) {
-  if (isDragging) {
-    const deltaX = event.clientX - startPosition.x;
-    const deltaY = event.clientY - startPosition.y;
-
-    const containerRect = spirographContainer.getBoundingClientRect();
-    const containerWidth = containerRect.width;
-    const containerHeight = containerRect.height;
-
-    const maxTranslateX = (containerWidth * zoomLevel - containerWidth) / 2;
-    const maxTranslateY = (containerHeight * zoomLevel - containerHeight) / 2;
-
-    currentTranslate.x = clamp(deltaX, -maxTranslateX, maxTranslateX);
-    currentTranslate.y = clamp(deltaY, -maxTranslateY, maxTranslateY);
-
-    spirographContainer.style.transform = `translate(${currentTranslate.x}px, ${currentTranslate.y}px)`;
-  }
-});
-
-spirographContainer.addEventListener("mouseup", function () {
-  isDragging = false;
-});
-
-spirographContainer.addEventListener("mouseleave", function () {
-  isDragging = false;
-});
-
 
   // Start animation
   animate();
